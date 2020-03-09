b8918ef810b56a3db48b79f02729008c8097eb3e
Nagear <faraon842@gmail.com>
2020-03-09 18:04:46 +0300

    added calc and output pie-statistic

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from ctypes import *
 
 
 class VKStatistic:
-    def __init__(self, api, choice, source_id):
+    def __init__(self, api, choice, source_id, dict_points):
         self.api = api
         self.choice = choice
         self.source_id = source_id
+        self.dict_points = dict_points
 
     def get_friends(self, user_id):
         return self.api.friends.get(user_id=user_id)['items']
@@ -55,7 +56,7 @@ class VKStatistic:
         return d
 
     def get_and_output_data(self):
-        if self.choice == 1:
+        if self.choice == 1:  #  Выбор источника id
             ids_list = self.get_members(self.source_id)
         elif self.choice == 2:
             ids_list = self.get_friends(self.source_id)
@@ -63,47 +64,65 @@ class VKStatistic:
             print("Неверный выбор. Перезапустите программу. ")
             exit()
 
-        res = {'sex': [], 'rel': [], 'dat': [], 'loc': []}
+        #  res = {'sex': [], 'rel': [], 'dat': [], 'loc': []}
+        res = dict()
+        for x in self.dict_points: #  Заполнение словаря кодами в зависимости от выбора пользователя
+            if self.dict_points[x][0]:
+                res[x] = []
+        print(res)
         data = dict()
         curr_year = datetime.datetime.now().year
 
         bar = progressbar.ProgressBar(maxval=len(ids_list)).start()
+        print(res.keys())
         for i, userid in enumerate(ids_list):
-            data[userid] = self.api.users.get(user_ids=userid, fields=['sex', 'bdate', 'city', 'relation'])
+            data[userid] = self.api.users.get(user_ids=userid, fields=list(res.keys()))
             q = data[userid][0]
-
-            res['sex'].append(int(q.get('sex', 0)))
-            res['rel'].append(int(q.get('relation', 0)))
-            z = q.get('bdate', 0)
-            if z == 0:  # Не указана вообще
-                res['dat'].append(z)
-            elif int(z.split('.')[-1]) > 31:  # Указан год
-                y = z.split('.')
-                res['dat'].append(curr_year - int(y[-1]))
-            else:  # Год не указан
-                res['dat'].append(0)
-            w = q.get('city', 0)
-            if type(w) is int:
-                res['loc'].append(w)
-            else:
-                res['loc'].append(w.get('id', 0))
+            if i == 5:
+                print(q)
+            for x in res:
+                if x == 'bdate':
+                    z = q.get(x, 0)
+                    if z == 0:  # Не указана вообще
+                        res[x].append(z)
+                    elif int(z.split('.')[-1]) > 31:  # Указан год
+                        y = z.split('.')
+                        res[x].append(curr_year - int(y[-1]))
+                    else:  # Год не указан
+                        res[x].append(0)
+                elif x == 'city':
+                    w = q.get(x, 0)
+                    if type(w) is int:
+                        res[x].append(w)
+                    else:
+                        res[x].append(w.get('id', 0))
+                else: #sex and relation
+                    res[x].append(int(q.get(x, 0)))
             bar.update(i)
         bar.finish()
         print("Сбор статистики окончен")
+        print(res)
 
-        d1 = dict(Counter(res['sex']))
-        d2 = dict(Counter(res['rel']))
-        d3 = dict(Counter(res['dat']))
-        d4 = dict(Counter(res['loc']))
+        dict_result = dict()
+        for x in res:  #Заполняем по ключам итогового отображения словарём с посчитанным числом каждого из значений
+            dict_result[self.dict_points[x][1]] = dict(Counter(res[x]))
 
-        d1 = self.change_keys(d1, ['не указано', 'жен', 'муж'])
-        d2 = self.change_keys(d2, ['не указано', 'не женат/не замужем', 'есть друг/есть подруга',
-                                   'помолвлен/помолевлена', 'женат/замужем', 'все сложно', 'в активном поиске',
-                                   'влюблен', 'в гражданском браке'])
-        d3 = self.change_keys(dict((k, v) for (k, v) in d3.items() if k < 100), ['не указано'])
-        d4 = self.change_keys(dict((k, v) for (k, v) in self.get_cities(d4).items() if v > 1), ['не указано'])
+        for x in dict_result:
+            if x == 'Пол':
+                dict_result[x] = self.change_keys(dict_result[x], ['не указано', 'жен', 'муж'])
+            elif x == 'Семейное положение':
+                dict_result[x] = self.change_keys(dict_result[x], ['не указано', 'не женат/не замужем',
+                                                                   'есть друг/есть подруга', 'помолвлен/помолевлена',
+                                                                   'женат/замужем', 'все сложно', 'в активном поиске',
+                                                                   'влюблен', 'в гражданском браке'])
+            elif x == 'Возраст':
+                dict_result[x] = self.change_keys(dict((k, v) for (k, v) in dict_result[x].items() if k < 100),
+                                                  ['не указано'])
+            elif x == 'Место проживания':
+                dict_result[x] = self.change_keys(dict((k, v) for (k, v) in self.get_cities(dict_result[x]).items()
+                                                       if v > 1), ['не указано'])
 
-        df = pd.DataFrame.from_dict({'Пол': d1, 'Семейное положение': d2, 'Возраст': d3, 'Место проживания': d4})
+        df = pd.DataFrame.from_dict(dict_result)
 
         df.plot.pie(subplots=True, legend=True, layout=(2, 2), figsize=(30, 30), startangle=-45)
         plt.tight_layout()
@@ -142,32 +161,45 @@ class Window(QWidget):
 
         self.checkbox1 = QCheckBox("Пол")
         self.checkbox1.setChecked(True)
+        self.checkbox1.code = "sex"
         layout.addWidget(self.checkbox1, 3, 0)
         self.checkbox2 = QCheckBox("Семейное положение")
+        self.checkbox2.setChecked(True)
+        self.checkbox2.code = "relation"
         layout.addWidget(self.checkbox2, 3, 1)
         self.checkbox3 = QCheckBox("Возраст")
+        self.checkbox3.setChecked(True)
+        self.checkbox3.code = "bdate"
         layout.addWidget(self.checkbox3, 3, 2)
         self.checkbox4 = QCheckBox("Место проживания")
+        self.checkbox4.setChecked(True)
+        self.checkbox4.code = "city"
         layout.addWidget(self.checkbox4, 3, 3)
 
         self.btn = QPushButton("Расчёт")
-        self.btn.clicked.connect(self.showData)
+        self.btn.clicked.connect(self.show_data)
         layout.addWidget(self.btn, 6, 9)
 
         self.setGeometry(windll.user32.GetSystemMetrics(0) // 4, windll.user32.GetSystemMetrics(1) // 4,
                          windll.user32.GetSystemMetrics(0) // 6, windll.user32.GetSystemMetrics(1) // 6)
         self.setWindowTitle("Статистика социальных сетей")
 
-    def showData(self):
+    def show_data(self):
         session = vk_api.VkApi(self.line_edit2.text(), self.line_edit3.text())
         session.auth()
         api = session.get_api()
-        choice = 1
+        choice = 0
         if self.radiobutton1.isChecked():
-            choice = 1
+            choice = 1 #  Выбор в качестве источника подписчиков группы
         elif self.radiobutton2.isChecked():
-            choice = 2
-        VKStatistic(api, choice, self.line_edit1.text()).get_and_output_data()
+            choice = 2 #  Выбор в качестве источника друзей человека
+        dict_p = dict() #  Заполнение словаря по ключам кодов значениями "Собирать ли данные по этому пункту" и "Итоговый текст отображения поля"
+        dict_p[self.checkbox1.code] = [True, self.checkbox1.text()] if self.checkbox1.isChecked() else [False, self.checkbox1.text()]
+        dict_p[self.checkbox2.code] = [True, self.checkbox2.text()] if self.checkbox2.isChecked() else [False, self.checkbox2.text()]
+        dict_p[self.checkbox3.code] = [True, self.checkbox3.text()] if self.checkbox3.isChecked() else [False, self.checkbox3.text()]
+        dict_p[self.checkbox4.code] = [True, self.checkbox4.text()] if self.checkbox4.isChecked() else [False, self.checkbox4.text()]
+
+        VKStatistic(api, choice, self.line_edit1.text(), dict_p).get_and_output_data()
 
 
 app = QApplication(sys.argv)
